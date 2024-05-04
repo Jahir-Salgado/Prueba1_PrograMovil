a68065bc8dd2e85d4b68bb395749050db8c5d4e3
Jahir Salgado <abielmejia0807@gmail.com>
2024-05-03 21:25:52 -0600

    Se actualizo para que fuera multicolor

--- lib/colorchangewidget.dart
+++ lib/colorchangewidget.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class ColorChangeWidget extends StatefulWidget {
-const ColorChangeWidget({super.key});
+  const ColorChangeWidget({super.key});
   @override
   _ColorChangeWidgetState createState() => _ColorChangeWidgetState();
 }
 
 class _ColorChangeWidgetState extends State<ColorChangeWidget> {
-  
   Color _currentColor = Colors.blue;
+
+  Color _getRandomColor() {
+    return Color.fromRGBO(
+      Random().nextInt(256),
+      Random().nextInt(256),
+      Random().nextInt(256),
+      1,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        
         setState(() {
-          _currentColor = _currentColor == Colors.blue? Colors.red : Colors.blue;
+          _currentColor =
+              _currentColor == Colors.blue ? _getRandomColor(): Colors.blue;
         });
       },
       child: Container(
